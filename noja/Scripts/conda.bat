44cf140ead70387d3b6553565357218bf1725877
Luis Mendez <lgmendez@gmail.com>
2017-04-13 20:36:05 -0500

    de nuevo

---
+++ noja/Scripts/conda.bat
@@ -0,0 +1,2 @@
+@echo off
+call "C:\Users\lgmen\Anaconda3\Scripts\conda" %*
---
+++ noja/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lgmen\google~1\proyec~1\noja\noja\noja\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
